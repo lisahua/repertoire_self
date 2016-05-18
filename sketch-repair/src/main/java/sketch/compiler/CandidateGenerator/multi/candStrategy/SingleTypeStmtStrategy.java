7205f394b19b6d8fce4ddd1218e5ffbfd6b1eac2
Lisa Hua <lisahua46@gmail.com>
2016-05-18 15:24:28 -0500

    [v1.0] sketchRep basic functionality to repair omission errors in assignment and conditions

--- sketch-repair/src/main/java/sketch/compiler/CandidateGenerator/multi/candStrategy/SingleTypeStmtStrategy.java
+++ sketch-repair/src/main/java/sketch/compiler/CandidateGenerator/multi/candStrategy/SingleTypeStmtStrategy.java
@@ -22,7 +22,8 @@ public class SingleTypeStmtStrategy extends CandidateStrategy {
 	public List<AtomicRunModel> generateModel(AtomicRunModel model, List<String> types, HashMap<String, Statement> typeInsertMap) {
 		List<AtomicRunModel> models = new ArrayList<AtomicRunModel>();
 		for (String type : types) {
-			for (int i = model.getLocation(); i > 0; i--)
+			for (int i = 0; i <model.getLocation(); i++)
+//			for (int i = model.getLocation(); i > 0; i--)
 				models.add(new AtomicRunModel(model.getFunc(), new ArrayList<String>(Arrays.asList(type)),
 						typeInsertMap.get(type), i));
 		}
